e7d9aaf8b0e3e247158b7e944829e9e154b66c8e
Captain Mcwiise <mcwiise@gmail.com>
2025-07-02 22:28:03 -0500

    feat: make it more cheaper

--- scripts/main.tf
+++ scripts/main.tf
@@ -2,14 +2,14 @@ module "stamper_vpc" {
   source = "../module/vpc"
   vpc_cidr_block = "192.168.0.0/16"
   vpc_name_tag = "stamper-vpc"
-  env_tag = "base-infra"
+  env_tag = "core-infra"
 }
 
 module "stamper_vpc_internet_gw" {
   source = "../module/internet_gw"
   vpc_id = module.stamper_vpc.id
   internet_gw_name_tag = "stamper-vpc-internet-gw"
-  env_tag = "base-infra"
+  env_tag = "core-infra"
   depends_on = [module.stamper_vpc]
 }
 
@@ -20,7 +20,7 @@ module "stamper_vpc_subnet_a_public" {
   map_public_ip_on_launch = true
   availability_zone = "us-east-1a"
   subnet_name_tag = "stamper-vpc-subnet-a-public"
-  env_tag = "base-infra"
+  env_tag = "core-infra"
   depends_on = [module.stamper_vpc]
 }
 
@@ -30,14 +30,14 @@ module "stamper_vpc_route_table" {
   public_subnet_id = module.stamper_vpc_subnet_a_public.id
   internet_gw_id = module.stamper_vpc_internet_gw.id
   route_table_name_tag = "stamper-vpc-route-table"
-  env_tag = "base-infra"
+  env_tag = "core-infra"
   depends_on = [
-    module.stamper_vpc_subnet_a_public,
-    module.stamper_vpc_internet_gw
+    module.stamper_vpc_subnet_a_public
+    #module.stamper_vpc_internet_gw
   ]
 }
 
-module "stamper_vpc_subnet_b_private" {
+/* module "stamper_vpc_subnet_b_private" {
   source = "../module/subnet"
   vpc_id = module.stamper_vpc.id
   cidr_block = "192.168.2.0/24"
@@ -46,16 +46,16 @@ module "stamper_vpc_subnet_b_private" {
   subnet_name_tag = "stamper-vpc-subnet-b-private"
   env_tag = "base-infra"
   depends_on = [module.stamper_vpc]
-}
+} */
 
-module "stamper_vpc_elastic_ip" {
+/* module "stamper_vpc_elastic_ip" {
   source = "../module/eip"
   domain = "vpc"
   eip_name_tag = "stamper-vpc-elastic-ip"
   env_tag = "base-infra"
-}
+} */
 
-module "stamper_vpc_nat_gw" {
+/* module "stamper_vpc_nat_gw" {
   source = "../module/nat_gw"
   elastic_ip_id = module.stamper_vpc_elastic_ip.id
   public_subnet_id = module.stamper_vpc_subnet_a_public.id
@@ -65,9 +65,9 @@ module "stamper_vpc_nat_gw" {
     module.stamper_vpc_elastic_ip,
     module.stamper_vpc_subnet_a_public
   ]
-}
+} */
 
-module "stamper_vpc_route_table_private" {
+/* module "stamper_vpc_route_table_private" {
   source = "../module/route_table_private"
   vpc_id = module.stamper_vpc.id
   private_subnet_id = module.stamper_vpc_subnet_b_private.id
@@ -78,7 +78,7 @@ module "stamper_vpc_route_table_private" {
     module.stamper_vpc_nat_gw,
     module.stamper_vpc_subnet_b_private
   ]
-}
+} */
 
 module "stamper_vpc_security_group" {
   source         = "../module/security_group"
@@ -86,12 +86,12 @@ module "stamper_vpc_security_group" {
   sg_vpc_id      = module.stamper_vpc.id
   sg_description = "Security group for ecs clusters"
   sg_ingress_rules = [
-    { from_port = 80, to_port = 80, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"], description = "Allow Web" },
+    { from_port = 3000, to_port = 3000, protocol = "tcp", cidr_blocks = ["0.0.0.0/0"], description = "Allow Microservices" },
   ]
   sg_egress_rules = [
     { from_port = 0, to_port = 0, protocol = "-1", cidr_blocks = ["0.0.0.0/0"], description = "Allow all outbound" },
   ]
-  env_tag = "base-infra"
+  env_tag = "core-infra"
   depends_on = [module.stamper_vpc]
 }
 
@@ -101,7 +101,7 @@ module "github_provider_openid" {
   openid_client_id_list = ["sts.amazonaws.com"]
   openid_thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
   openid_name_tag = "github-provider-open-id"
-  env_tag = "base-infra"
+  env_tag = "core-infra"
 }
 
 module "stamper_role_ecs_tasks_execution" {
@@ -112,7 +112,7 @@ module "stamper_role_ecs_tasks_execution" {
     "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
     "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
   ]
-  env_tag = "base-infra"
+  env_tag = "core-infra"
 }
 
 module "stamper_policy_ecr_push_and_pull" {
@@ -129,17 +129,17 @@ module "stamper_role_github_actions" {
   # apply twice module.std_ecr_iam_policy.iam_policy_arn to ensure it is created before use
   # policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess"]
   policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess", module.stamper_policy_ecr_push_and_pull.arn]
-  env_tag = "base-infra"
+  env_tag = "core-infra"
 }
 
-module "stamper_std_onboarding_api_ecr" {
+module "stamper_cnd_onboarding_api_ecr" {
   source = "../module/ecr"
-  repository_name = "stamper/std-onboarding-api"
-  env_tag = "base-infra"
+  repository_name = "stamper/cnd-onboarding-api"
+  env_tag = "core-infra"
 }
 
-module "std_ecs_cluster_staging" {
+module "cnd_ecs_cluster_staging" {
   source = "../module/ecs"
-  cluster_name = "std-ecs-cluster-staging"
+  cluster_name = "cnd-ecs-cluster-staging"
   env_tag = "staging"
 }
