6db5d4794be64e98642e4002cba8652ef36e9742
Captain Mcwiise <mcwiise@gmail.com>
2025-11-07 17:22:07 -0500

    feat: provision vps with ansible

--- aws/stacks/lightsail/outputs.tf
+++ aws/stacks/lightsail/outputs.tf
@@ -5,3 +5,7 @@ output "stamper_ligthsail_vps_ssh_key_name" {
 output "stamper_ligthsail_vps_instance_name" {
   value = module.stamper_lightsail_vps.instance_name
 }
+
+output "stamper_ligthsail_vps_public_ip_address" {
+  value = module.stamper_lightsail_vps.public_ip_address
+}
